c58997474e1cb3edd1f567dce20cd81db9d68e4c
102782 <fuyajyoured@yahoo.co.jp>
2013-05-05 03:39:16 +0900

    sampleのformデザイン修正

--- sample/Form1.Designer.cs
+++ sample/Form1.Designer.cs
@@ -33,10 +33,16 @@
             this.windowText = new System.Windows.Forms.TextBox();
             this.address = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.button1.Location = new System.Drawing.Point(11, 200);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(261, 50);
@@ -47,38 +53,84 @@
             // 
             // exeFile
             // 
-            this.exeFile.Location = new System.Drawing.Point(11, 12);
+            this.exeFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.exeFile.Location = new System.Drawing.Point(89, 12);
             this.exeFile.Name = "exeFile";
-            this.exeFile.Size = new System.Drawing.Size(261, 19);
+            this.exeFile.Size = new System.Drawing.Size(183, 19);
             this.exeFile.TabIndex = 1;
             // 
             // windowText
             // 
-            this.windowText.Location = new System.Drawing.Point(12, 37);
+            this.windowText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.windowText.Location = new System.Drawing.Point(89, 37);
             this.windowText.Name = "windowText";
-            this.windowText.Size = new System.Drawing.Size(260, 19);
+            this.windowText.Size = new System.Drawing.Size(183, 19);
             this.windowText.TabIndex = 2;
             // 
             // address
             // 
-            this.address.Location = new System.Drawing.Point(11, 62);
+            this.address.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.address.Location = new System.Drawing.Point(89, 62);
             this.address.Name = "address";
-            this.address.Size = new System.Drawing.Size(261, 19);
+            this.address.Size = new System.Drawing.Size(183, 19);
             this.address.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 84);
+            this.label1.Location = new System.Drawing.Point(93, 116);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 12);
             this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "exeFile :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "windowText :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "address :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "read :";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.address);
             this.Controls.Add(this.windowText);
@@ -98,6 +150,10 @@
         private System.Windows.Forms.TextBox windowText;
         private System.Windows.Forms.TextBox address;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
